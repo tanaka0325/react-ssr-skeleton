6363c7ac483922c43e71f206d662742fdf531983
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-06-06 23:05:34 +0900

    port変更

--- src/js/server.js
+++ src/js/server.js
@@ -42,7 +42,7 @@ function renderPage(appHtml) {
   `
 }
 
-var PORT = process.env.PORT || 8888
+var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
 })
