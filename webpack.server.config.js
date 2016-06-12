acad858c716348ba43ea60426cfdc4e2d50470b0
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-06-12 22:27:36 +0900

    install webpack-node-extarnals.

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,7 +1,10 @@
+var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
     name: 'server',
     entry: __dirname + "/src/js/server.js",
     target: 'node',
+    externals: [nodeExternals()],
     output: {
       path: __dirname,
       filename: 'server.bundle.js',
